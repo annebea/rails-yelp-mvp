b371fa8f289d2299a77402a715ff6621ee7ea65c
Anne Mabire <a.b.m.mabire@gmail.com>
2020-04-01 21:33:12 +0200

    finished

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,6 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
+  accepts_nested_attributes_for :reviews
 
   category = %w[chinese italian japanese french belgian]
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Restaurant.destroy_all
+
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Company.name,
+    phone_number: Faker::PhoneNumber.phone_number,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    category: %w[chinese italian japanese french belgian].sample
+  )
+  restaurant.save!
+end
